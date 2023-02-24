31dd3990b4f72d31c505e4615c84f0441b7ec84f
Christian Beckham <58891954+christianbeckham@users.noreply.github.com>
2023-02-23 18:13:48 -0600

    feat: added run method to start game, method to get player name from user input, and updated welcome message method type

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Game {
 
-    public static void displayWelcome() {
-        System.out.println("Game has started!");
+    private HumanPlayer playerOne;
+    public Game() {}
+
+    public void displayWelcome() {
+        System.out.println("Game has started! \n");
+    }
+
+    public void run() {
+        String name = this.getPlayerNameInput();
+        this.playerOne = new HumanPlayer(name);
+        String choice = playerOne.makeChoice();
+        System.out.println(playerOne.getName() + " picked " + choice);
+    }
+
+    private String getPlayerNameInput() {
+        System.out.print("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
     }
 }
