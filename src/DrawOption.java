55db117ce2a9a662d8d13892050a067718e72564
Christian Beckham <58891954+christianbeckham@users.noreply.github.com>
2023-02-23 18:10:50 -0600

    feat: created abstract class with static method to get choices

---
+++ src/DrawOption.java
@@ -0,0 +1,7 @@
+abstract class DrawOption {
+    private static String[] items = {"Rock", "Paper", "Scissors", "Lizard", "Spock"};
+
+    public static String[] getItems() {
+        return items;
+    }
+}
