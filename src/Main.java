1a58c04c33b37d12af1026aa490641e6898ee387
Christian Beckham <58891954+christianbeckham@users.noreply.github.com>
2023-02-27 19:05:37 -0600

    feat: simplified main method by removing the game welcome method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to RPSLS \n");
-
         Game game = new Game();
-        game.displayWelcome();
         game.run();
-
     }
 }
